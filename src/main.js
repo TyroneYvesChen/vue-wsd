b499c655d8d9ac89d36367a874f78cf999972805
TyroneYvesChen <243307272@qq.com>
2017-09-20 10:17:40 +0800

    添加了mint-ui fastclick better-scroll

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
